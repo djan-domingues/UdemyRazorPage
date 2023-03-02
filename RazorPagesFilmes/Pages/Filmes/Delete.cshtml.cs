4c8061f00848c79babf1d43f5ced2962396ad64e
Felipe Januário Domingues <felipej.domingues@gmail.com>
2023-03-01 22:22:42 -0300

    Curso 0 a pro dotnet

---
+++ RazorPagesFilmes/Pages/Filmes/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilmes.Data;
+using RazorPagesFilmes.Modelo;
+
+namespace RazorPagesFilmes.Pages.Filmes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorPagesFilmes.Data.RazorPagesFilmesContext _context;
+
+        public DeleteModel(RazorPagesFilmes.Data.RazorPagesFilmesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Filme Filme { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Filme = await _context.Filme.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Filme == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Filme = await _context.Filme.FindAsync(id);
+
+            if (Filme != null)
+            {
+                _context.Filme.Remove(Filme);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
